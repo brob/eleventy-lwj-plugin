aff4fd7ea9d03a7cac00a7c492a3ae760e9eaa86
Bryan Robinson <bryanlrobinson@gmail.com>
2020-07-29 21:52:55 -0400

    Removes posts collection

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,4 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addCollection("lwjPosts", function(collectionApi) {
-        return collectionApi.getFilteredByTag("lwjPost");
-    });
-    
 
 };
